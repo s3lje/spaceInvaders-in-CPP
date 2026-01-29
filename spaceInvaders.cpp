52a7136aea52f1d79e40e94a506e5fa5cb21a80c
s3lje <emil.seljestokken@outlook.com>
2026-01-29 10:24:51 +0100

    lagt til spillersprite og bevegelse, samt animasjon på fiende

--- spaceInvaders.cpp
+++ spaceInvaders.cpp
@@ -5,16 +5,44 @@
 
 
 // CPU buffer
-struct Buffer{
+struct Buffer {
     size_t width, height;
     uint32_t* data;
 };
 
 // Sprite
-struct Sprite{
+struct Sprite {
     size_t width, height;
     uint8_t* data;
 };
+
+struct SpriteAnimation {
+    bool loop;
+    size_t num_frames;
+    size_t frame_duration;
+    size_t time;
+    Sprite** frames;
+};
+
+struct Alien {
+    size_t x, y;
+    uint8_t type;
+};
+
+struct Player {
+    size_t x, y;
+    size_t life;
+};
+
+struct Game {
+    size_t width, height; 
+    size_t num_aliens;
+    Alien* aliens;
+    Player player;
+};
+
+bool game_running = 1; 
+int move_dir      = 0;
 
 /* =====================
      SHADERS
@@ -47,6 +75,7 @@ const char* fragment_shader_src =
     "}\n";
 
 void error_callback(int, const char*);
+void key_callback(GLFWwindow*, int, int, int, int);
 void validate_shader(GLuint, const char*);
 bool validate_program(GLuint);
 uint32_t rgb_to_uint32(uint8_t, uint8_t, uint8_t);
@@ -77,9 +106,12 @@ int main(){
         glfwTerminate();
         return EXIT_FAILURE;
     }
-    
+    glfwSetKeyCallback(window, key_callback);
+
     // Putter vinduet til OPENGL konteksten
     glfwMakeContextCurrent(window);
+    
+    glfwSwapInterval(1);
 
     // Initialiserer GLEW
     glewExperimental = GL_TRUE;
@@ -92,10 +124,10 @@ int main(){
     }
 
     // Lager CPU bufferen
-    const size_t buffer_width  = 320;
-    const size_t buffer_height = 240; 
+    const size_t buffer_width  = 224;
+    const size_t buffer_height = 256; 
     
-    uint32_t clear_color = rgb_to_uint32(0, 128, 0);
+    uint32_t clear_color = rgb_to_uint32(0, 0, 0);
     Buffer buffer;
     buffer.width  = buffer_width;
     buffer.height = buffer_height;
@@ -103,10 +135,10 @@ int main(){
     buffer_clear(&buffer, clear_color);
 
     // Alien Sprite
-    Sprite alien;
-    alien.width = 11;
-    alien.height = 8;
-    alien.data = new uint8_t[11 * 8]{
+    Sprite alien_sprite;
+    alien_sprite.width = 11;
+    alien_sprite.height = 8;
+    alien_sprite.data = new uint8_t[11 * 8]{
         0,0,1,0,0,0,0,0,1,0,0,
         0,0,0,1,0,0,0,1,0,0,0,
         0,0,1,1,1,1,1,1,1,0,0,
@@ -115,7 +147,63 @@ int main(){
         1,0,1,1,1,1,1,1,1,0,1,
         1,0,1,0,0,0,0,0,1,0,1,
         0,0,0,1,1,0,1,1,0,0,0};
+
+    Sprite alien_sprite1;
+    alien_sprite1.width = 11;
+    alien_sprite1.height = 7;
+    alien_sprite1.data = new uint8_t[11 * 8]{
+    0,0,1,0,0,0,0,0,1,0,0,
+    1,0,0,1,0,0,0,1,0,0,1, 
+    1,0,1,1,1,1,1,1,1,0,1, 
+    1,1,1,0,1,1,1,0,1,1,1, 
+    1,1,1,1,1,1,1,1,1,1,1, 
+    0,1,1,1,1,1,1,1,1,1,0, 
+    0,0,1,0,0,0,0,0,1,0,0, 
+    0,1,0,0,0,0,0,0,0,1,0};
     
+    SpriteAnimation* alien_animation = new SpriteAnimation;
+    alien_animation->loop = true;
+    alien_animation->num_frames = 2;
+    alien_animation->frame_duration = 10;
+    alien_animation->time = 0;
+
+    alien_animation->frames = new Sprite*[2];
+    alien_animation->frames[0] = &alien_sprite;
+    alien_animation->frames[1] = &alien_sprite1;
+
+
+    // Player Sprite
+    Sprite player_sprite;
+    player_sprite.width = 11;
+    player_sprite.height = 7;
+    player_sprite.data = new uint8_t[11 * 7]{
+        0,0,0,0,0,1,0,0,0,0,0,
+        0,0,0,0,1,1,1,0,0,0,0,
+        0,0,0,0,1,1,1,0,0,0,0,
+        0,1,1,1,1,1,1,1,1,1,0,
+        1,1,1,1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,1,1,1};
+
+
+    // Initialiser Game strukten
+    Game game;
+    game.width = buffer_width;
+    game.height = buffer_height;
+    game.num_aliens = 55;
+    game.aliens = new Alien[game.num_aliens];
+
+    game.player.x = 112 - 5;
+    game.player.y = 32;
+    game.player.life = 3; 
+
+    for (size_t yi{0}; yi < 5; ++yi){
+        for (size_t xi{0}; xi < 11; ++xi){
+            game.aliens[yi * 11 + xi].x = 16 * xi + 20;
+            game.aliens[yi * 11 + xi].y = 17 * yi + 128;
+        }
+    }
+
 
     //OpenGl objekter
     GLuint vao;
@@ -166,15 +254,43 @@ int main(){
 
 
     // Spill løkken 
-    while (!glfwWindowShouldClose(window)){
+    while (!glfwWindowShouldClose(window) && game_running){
         buffer_clear(&buffer, clear_color);
+        
+        // Draw Alien Sprite
+        for (size_t ai = 0; ai < game.num_aliens; ++ai){
+            const Alien& alien = game.aliens[ai];
+            size_t current_frame = alien_animation->time / alien_animation->frame_duration;
+            const Sprite& sprite = *alien_animation->frames[current_frame]; 
+            buffer_sprite_draw(&buffer, sprite,
+                    alien.x, alien.y, rgb_to_uint32(255, 0, 0));
+        }
 
-        buffer_sprite_draw(
-            &buffer,
-            alien,
-            112, 128,
-            rgb_to_uint32(128, 0, 0)
-        );
+        buffer_sprite_draw(&buffer, player_sprite,
+                game.player.x, game.player.y, rgb_to_uint32(255, 255, 255));
+        
+
+        // Update Alien Animation 
+        ++alien_animation->time;
+        if (alien_animation->time == alien_animation->num_frames *
+                alien_animation->frame_duration){
+            if (alien_animation->loop) alien_animation->time = 0;
+            else {
+                delete alien_animation;
+                alien_animation = nullptr;
+            }
+        }
+
+        // Bevegelses logikk
+        int player_move_dir = 2 * move_dir;
+        if (player_move_dir != 0){
+            if (game.player.x + player_sprite.width + player_move_dir >= game.width){
+                game.player.x = game.width - player_sprite.width;
+            } else if ((int)game.player.x + player_move_dir <= 0){
+                game.player.x = 0; 
+            } else
+                game.player.x += player_move_dir; 
+        }
 
         glTexSubImage2D(
             GL_TEXTURE_2D, 0, 0, 0,
@@ -182,14 +298,14 @@ int main(){
             GL_RGBA, GL_UNSIGNED_INT_8_8_8_8,
             buffer.data
         );
-
+        
         glDrawArrays(GL_TRIANGLES, 0, 3);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     delete[] buffer.data;
-    delete[] alien.data;
+    delete[] alien_sprite.data;
 
     std::cout << "Exiting...\n";
     glfwDestroyWindow(window);
@@ -200,6 +316,24 @@ int main(){
 
 void error_callback(int error, const char* description){
     fprintf(stderr, "Error: %s\n", description);
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
+    switch (key){
+        case GLFW_KEY_ESCAPE:
+            if (action == GLFW_PRESS) game_running = false;
+            break;
+        case GLFW_KEY_RIGHT:
+            if (action == GLFW_PRESS) move_dir += 1;
+            else if (action == GLFW_RELEASE) move_dir -= 1;
+            break;
+        case GLFW_KEY_LEFT:
+            if (action == GLFW_PRESS) move_dir -= 1;
+            else if (action == GLFW_RELEASE) move_dir += 1;
+            break;
+        default:
+            break;
+    }
 }
 
 void validate_shader(GLuint shader, const char* name = ""){
